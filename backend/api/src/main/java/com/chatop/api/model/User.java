027a1554ed22b923cd5f33546b44cce96a847d53
Keijur0 <tran.qlam@gmail.com>
2024-05-13 19:38:58 +0200

    User, Rental, Messages models created / app properties set up / Open API (swagger) dependency added

---
+++ backend/api/src/main/java/com/chatop/api/model/User.java
@@ -0,0 +1,35 @@
+package com.chatop.api.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    @Column(name="created_at")
+    private Timestamp createdAt;
+
+    @Column(name="updated_at")
+    private Timestamp updatedAt;
+
+}
